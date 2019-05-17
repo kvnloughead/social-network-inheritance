6208d80a3a84da723fc3f7160551cd2b5d7d3267
kvnloughead <37159177+kvnloughead@users.noreply.github.com>
2019-05-17 09:55:22 -0400

    All tests working.

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,49 @@
 from datetime import datetime
 
-# Please remove the comments and 
+# Please remove the comments and
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
+
+    def _timestring(self):
+        return '{}, {} {}, {}'.format(self.timestamp.strftime("%A"),
+                                             self.timestamp.strftime("%b"),
+                                             self.timestamp.day, self.timestamp.year)
+
+    def __str__(self):
+        s = '@{} {}: "{}"\n\t{}'
+        return s.format(self.user.first_name, self.user.last_name,
+                        self.text, self._timestring())
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super().__init__(text, timestamp)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text, timestamp)
+        self.image_url = image_url
 
+    def __str__(self):
+        base = super().__str__()
+        return '\n\t{}\n\t'.format(self.image_url).join(base.split('\n\t'))
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        base = super().__str__().replace(self.user.last_name, 'Checked In')
+        return '\n\t{}, {}\n\t'.format(self.latitude, self.longitude).join(base.split('\n\t'))
