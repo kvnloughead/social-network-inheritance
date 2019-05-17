36850d5a739b77c071fe8fd7a16be6bb1690d75a
kvnloughead <37159177+kvnloughead@users.noreply.github.com>
2019-05-17 11:04:54 -0400

    All tests working.

--- social_network/posts.py
+++ social_network/posts.py
@@ -26,24 +26,24 @@ class Post(object):
 
 class TextPost(Post):
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost, self).__init__(text, timestamp)
 
 
 class PicturePost(Post):
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp)
+        super(PicturePost, self).__init__(text, timestamp)
         self.image_url = image_url
 
     def __str__(self):
-        base = super().__str__()
+        base = super(PicturePost, self).__str__()
         return '\n\t{}\n\t'.format(self.image_url).join(base.split('\n\t'))
 
 class CheckInPost(Post):
     def __init__(self, text, latitude, longitude, timestamp=None):
-        super().__init__(text, timestamp)
+        super(CheckInPost, self).__init__(text, timestamp)
         self.latitude = latitude
         self.longitude = longitude
 
     def __str__(self):
-        base = super().__str__().replace(self.user.last_name, 'Checked In')
+        base = super(CheckInPost, self).__str__().replace(self.user.last_name, 'Checked In')
         return '\n\t{}, {}\n\t'.format(self.latitude, self.longitude).join(base.split('\n\t'))
